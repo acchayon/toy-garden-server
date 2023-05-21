748b757a7cd60a0fcd4b5f23dfce08f2b6f9f905
acchayon <adhikaryac81@gmail.com>
2023-05-21 17:32:53 +0600

    added delete method successfully

--- index.js
+++ index.js
@@ -81,6 +81,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete
+    app.delete('/toys/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.deleteOne(query);
+      res.send(result)
+    })
+
 
 
 
